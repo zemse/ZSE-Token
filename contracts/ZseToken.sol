41e940668598cb5c5b0df8538edac3dd6d54731f
zemse <sohamzemse143@gmail.com>
2019-05-23 13:44:20 +0530

    add approve and transferFrom methods

--- contracts/ZseToken.sol
+++ contracts/ZseToken.sol
@@ -7,11 +7,18 @@ contract ZseToken {
 
     uint256 public totalSupply;
 
-    mapping (address => uint256) public balanceOf;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed _from,
         address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
         uint256 _value
     );
 
@@ -27,6 +34,24 @@ contract ZseToken {
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
 
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
--- test/ZseToken.test.js
+++ test/ZseToken.test.js
@@ -25,7 +25,7 @@ contract('ZseToken', accounts => {
       1000000
     );
   });
-  it('transfer: transfers ownership of tokens', async() => {
+  it('transfers ownership of tokens', async() => {
     const initial0 = numberify(await instance.balanceOf(accounts[0]));
     const initial1 = numberify(await instance.balanceOf(accounts[1]));
 
@@ -37,16 +37,15 @@ contract('ZseToken', accounts => {
     assert.equal(initial0 - final0, final1 - initial1);
     assert.equal(initial0 - final0, 100);
   });
-  it('transfer: fires an event', async() => {
+  it('transfer fires an event', async() => {
     const receipt = await instance.transfer(accounts[1], 100);
     assert.equal(receipt.logs.length, 1, 'triggers an event');
     assert.equal(receipt.logs[0].event, 'Transfer', 'should be the Transfer event');
     assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the sender');
     assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the receiver');
     assert.equal(receipt.logs[0].args._value, 100, 'logs the value');
-
   });
-  it('transfer: throws an error if insufficient balance', async() => {
+  it('throws revert error if insufficient balance during transfer', async() => {
     try {
       // await instance.transfer(accounts[2], 100, {from: accounts[1]});
       await instance.transfer.call(accounts[1], 999999999, {from: accounts[0]});
@@ -55,4 +54,42 @@ contract('ZseToken', accounts => {
     }
     assert(false);
   });
+  it('approves token for delegate transfer', async() => {
+    const success = await instance.approve.call(accounts[1], 100);
+    assert.equal(success, true, 'it returns true');
+    const receipt = await instance.approve(accounts[1], 100);
+    assert.equal(receipt.logs.length, 1, 'triggers an event');
+    assert.equal(receipt.logs[0].event, 'Approval', 'should be the Approval event');
+    assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the owner');
+    assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the spender');
+    assert.equal(receipt.logs[0].args._value, 100, 'logs the value');
+    const allowance = numberify(await instance.allowance(accounts[0], accounts[1]));
+    assert.equal(allowance, 100, 'sets allowance properly');
+  });
+  it('handles delegate token transfer', async() => {
+    const [ fromAccount, toAccount, spendingAccount ] = [ accounts[2], accounts[3], accounts[4] ];
+
+    await instance.transfer(fromAccount, 100);
+
+    const fromBalance = numberify(await instance.balanceOf(fromAccount));
+    const toBalance = numberify(await instance.balanceOf(toAccount));
+
+    await instance.approve(spendingAccount, 10, {from: fromAccount});
+    await instance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount});
+
+    assert.equal(
+      fromBalance - numberify(await instance.balanceOf(fromAccount)),
+      10,
+      'deducts the amount from fromAccount'
+    );
+    assert.equal(
+
+    );
+    assert.equal(
+      numberify(await instance.balanceOf(toAccount)) - toAccount,
+      10,
+      'adds the amount to the toAccount'
+    );
+  });
+
 });
